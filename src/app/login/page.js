3267a0b3252b96cb0c8dba56dd8685a51d1d9dfd
cricamore <cristian.camilo.montano@correounivalle.edu.co>
2024-03-04 23:18:15 -0500

    first commit

---
+++ src/app/login/page.js
@@ -0,0 +1,50 @@
+import { AppBar, Button, Container, Paper, TextField, Toolbar } from '@mui/material'
+import React from 'react'
+import Image from "next/image";
+import "./page.css";
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
+
+export default function Login() {
+  return (
+    <>
+      <head>
+        <title>Login</title>
+      </head>
+
+      <body>
+        <div>
+          {/* <AppBar className='colors' position='fixed'>
+            <Toolbar>
+              <Image
+                src={"/images/GymGPT.png"}
+                width={71}
+                height={71}
+              />
+            </Toolbar>
+          </AppBar> */}
+        </div>
+        <div id='divImage' style={{marginTop: "80px"}}>
+          <Paper id='paper' elevation={6}>
+            <img
+              id='gymgptLogo'
+              src={"/images/GymGPT.png"}/>
+            <h1>¡Bienvenida!</h1>
+            <p>Cumple tus objetivos con la ayuda de un entrenador virtual potenciado con inteligencia artificial.</p>
+            <Grid2 container direction="column" spacing={2}>
+              <Grid2 item>
+                <TextField id="outlined-basic" label="Correo electrónico" variant="outlined" fullWidth />
+              </Grid2>
+              <Grid2 item>
+                <TextField id="outlined-basic" label="Contraseña" variant="outlined" fullWidth />
+              </Grid2>
+              <Grid2 item>
+                <Button className='colors boton' variant="contained" color="primary" fullWidth> Iniciar Sesión </Button>
+              </Grid2>
+            </Grid2>
+          </Paper>  
+        </div>
+      </body>
+
+    </>
+  )
+}
