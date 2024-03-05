385537886adbfd0b94fd228dc83dbc53e8b853ea
cricamore <cristian.camilo.montano@correounivalle.edu.co>
2024-03-05 18:14:38 -0500

    login and register

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,14 +1,14 @@
-import { AppBar, Button, Container, Paper, TextField, Toolbar } from '@mui/material'
 import React from 'react'
+import { AppBar, Button, Container, Paper, TextField, Toolbar } from '@mui/material'
 import Image from "next/image";
 import "./page.css";
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 
-export default function Login() {
+export default function Register() {
   return (
     <>
       <head>
-        <title>Login</title>
+        <title>Register</title>
       </head>
 
       <body>
@@ -28,25 +28,21 @@ export default function Login() {
             <img
               id='gymgptLogo'
               src={"/images/GymGPT.png"}/>
-            <h1>¡Bienvenida!</h1>
+            <h1>Registro</h1>
             <p>Cumple tus objetivos con la ayuda de un entrenador virtual potenciado con inteligencia artificial.</p>
             <Grid2 container direction="column" spacing={2}>
-            <Grid2 item>
-                <p> ¿No tienes una cuenta? <a href='/register'>Regístrate</a> </p>
+              <Grid2 item>
+                <TextField id="outlined-basic" label="Nombre" variant="outlined" fullWidth />
               </Grid2>
               <Grid2 item>
                 <TextField id="outlined-basic" label="Correo electrónico" variant="outlined" fullWidth />
               </Grid2>
               <Grid2 item>
-                <TextField id="outlined-basic" label="Contraseña" variant="outlined" type='password' fullWidth />
+                <TextField id="outlined-basic" label="Contraseña" variant="outlined" type="password" fullWidth />
               </Grid2>
               <Grid2 item>
-                <Button className='colors boton' variant="contained" color="primary" fullWidth> Iniciar Sesión </Button>
+                <Button className='colors boton' variant="contained" color="primary" fullWidth> Registrarse </Button>
               </Grid2>
-              <Grid2 item>
-                <a>Restablecer contraseña</a>
-              </Grid2>
-              
             </Grid2>
           </Paper>  
         </div>
