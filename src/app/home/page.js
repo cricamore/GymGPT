2e72a8fbb3a85a5fdd769dc35502e8ca3f9190d7
Juan Esteban <betancourtjuanes@gmail.com>
2024-03-12 10:27:16 -0500

    update

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -19,7 +19,7 @@ export default function Home() {
       const userMessage = { text: newMessage, type: "user" };
       setMessages((prevMessages) => [...prevMessages, userMessage]);
 
-      const response = await fetch('http://localhost:3000/api/chat', {
+      const response = await fetch('/api/chat', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
